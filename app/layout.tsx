baa26a027c67993f8eaceadc8ccc3ddfb4b7c45d
Sholo <editor.sholo@gmail.com>
2024-12-12 13:23:42 +0600

    Add subset

--- app/layout.tsx
+++ app/layout.tsx
@@ -44,7 +44,8 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&family=Noto+Sans+Bengali:wght@100..900&display=swap" rel="stylesheet"/>      </head>
+        <link href="https://fonts.googleapis.com/css2?family=Hind+Siliguri:wght@300;400;500;600;700&family=Noto+Sans+Bengali:wght@100..900&display=swap" data-subset="bengali,latin" rel="stylesheet"/>      
+      </head>
       <body
         className={cn("min-h-screen font-body antialiased", fontSans.className)}
       >
